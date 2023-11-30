26ba6bc7ca30b72e7f47f959ab9f35ea0368e5cd
PavelLSmirnov <pavel1201s@yandex.ru>
2023-11-30 12:17:51 +0300

    feat(1Lab): intit 1 lab

---
+++ tasks/task_1/smirnov_p_most_different_neighbor_elements/most_different_vector_elements.h
@@ -0,0 +1,14 @@
+#ifndef MODULES_TASK_1_KORNEV_N_MOST_DIFFERENT_VECTOR_ELEMENTS_MOST_DIFFERENT_VECTOR_ELEMENTS_H_
+#define MODULES_TASK_1_KORNEV_N_MOST_DIFFERENT_VECTOR_ELEMENTS_MOST_DIFFERENT_VECTOR_ELEMENTS_H_
+
+// Copyright 2019 Kornev Nikita
+
+#include <vector>
+
+using namespace std;
+
+vector<int> getRandVect(int n);
+int single_find(vector<int> v);
+int mpi_find(vector<int> v);
+
+#endif  // MODULES_TASK_1_KORNEV_N_MOST_DIFFERENT_VECTOR_ELEMENTS_MOST_DIFFERENT_VECTOR_ELEMENTS_H_
